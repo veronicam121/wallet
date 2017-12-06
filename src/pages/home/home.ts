cc3ab28309535231b1319a7836a7bd900fe869ed
veronicam121 <veronicam121@gmail.com>
2017-12-06 11:00:29 -0400

    Pushing to Repo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,10 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  cuenta:string;
 
   constructor(public navCtrl: NavController) {
-
+    this.cuenta="1BPmau8ewds343Bgsds34jsS2fd342saTscqS";
   }
 
 }
